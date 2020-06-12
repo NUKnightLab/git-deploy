f7af8504a1bfefc3d754c606d4ed4b5f9c129885
Scott Bradley <scottbradley.mail@gmail.com>
2020-06-11 19:10:13 -0500

    fixes to python application deployment

--- git-deploy.py
+++ git-deploy.py
@@ -182,9 +182,12 @@ def deploy(env, verbose=False, project_virtualenv=None, playbook=None):
         if COMMON_CONFIG.get('gitdeploy_version') == '1.0.5':
             if COMMON_CONFIG.get('docker_compose_file'):
                 build_containers(env, verbose)
-            ansible_playbook(env, 'deploy.web.1.05.yml', verbose)
+            else:
+                print('No containers to build. Deploying legacy application ...')
+                deploy_application(env, verbose)
+                deploy_static(env, verbose, project_virtualenv)
+                ansible_playbook(env, 'deploy.web.1.05.yml', verbose)
         else:
-            print('No containers to build. Deploying legacy application ...')
             deploy_application(env, verbose)
             deploy_static(env, verbose, project_virtualenv)
             deploy_web(env, verbose)
