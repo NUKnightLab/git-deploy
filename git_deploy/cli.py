fc935ac8cfdbb6d2ea165219f90eba8ccfa2b908
Scott Bradley <scottbradley.mail@gmail.com>
2020-09-25 22:21:36 -0500

    adds vault subcommand

--- git_deploy/cli.py
+++ git_deploy/cli.py
@@ -14,7 +14,6 @@ from . import __version__
 from dotenv import load_dotenv
 
 
-
 WARNING = '\033[93m'
 FAIL = '\033[91m'
 ENDC = '\033[0m'
@@ -75,6 +74,13 @@ def ansible_playbook(env, playbook, *ansible_args, **kwargs):
     call(command)
 
 
+def ansible_vault(env, command):
+    project_name = COMMON_CONFIG['project_name']
+    vault_file = os.path.join(get_vault_dir(), project_name, f'vault.{env}.yml')
+    command = f'ansible-vault {command} {vault_file}'
+    call(command)
+
+
 def playbook_path(name):
     path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'ansible')
     return os.path.join(path, name)
@@ -116,17 +122,52 @@ import click
 from click_option_group import optgroup, RequiredMutuallyExclusiveOptionGroup
 
 
-@click.command(context_settings=dict(
-    ignore_unknown_options=True,
-    allow_extra_args=True,
-))
-@click.pass_context
+class DefaultCommandGroup(click.Group):
+    """Group with default command. From: https://stackoverflow.com/a/52069546
+    """
+    def command(self, *args, **kwargs):
+        default_command = kwargs.pop('default_command', False)
+        if default_command and not args:
+            kwargs['name'] = kwargs.get('name', '<>')
+        decorator = super(
+            DefaultCommandGroup, self).command(*args, **kwargs)
+        if default_command:
+            def new_decorator(f):
+                cmd = decorator(f)
+                self.default_command = cmd.name
+                return cmd
+            return new_decorator
+        return decorator
+
+    def resolve_command(self, ctx, args):
+        try:
+            return super(
+                DefaultCommandGroup, self).resolve_command(ctx, args)
+        except click.UsageError:
+            args.insert(0, self.default_command)
+            return super(
+                DefaultCommandGroup, self).resolve_command(ctx, args)
+
+
+context_settings = {
+    'ignore_unknown_options': True,
+    'allow_extra_args': True
+}
+
+@click.group(cls=DefaultCommandGroup)
 @click.version_option()
+@click.pass_context
+def cli(ctx):
+    pass
+
+
+@cli.command(default_command=True, context_settings=context_settings)
+@click.pass_context
 @click.argument('env')
 @click.argument('project-version')
 @click.option('-p', '--playbook', required=False,
     help='Specify a playbook to run rather than a project deployment.')
-def cli(ctx, env, project_version, playbook):
+def default(ctx, env, project_version, playbook):
     """Deploy to environment ENV, PROJECT_VERSION of the current project repository.
 
     ENV: A configuration evironment with hosts configured in the ansible inventory.
@@ -142,3 +183,18 @@ def cli(ctx, env, project_version, playbook):
         ansible_playbook(env, playbook, *ansible_args)
     else:
         deploy(env, project_version, *ansible_args)
+
+
+@cli.command()
+@click.pass_context
+@click.argument('command')
+@click.argument('env')
+def vault(ctx, command, env):
+    """Invoke an ansible-vault command on the env-specific vault file for this
+    project.
+
+    COMMAND: ansible-vault command (create|decrypt|edit|view|encrypt|rekey)
+
+    ENV: A configuration evironment with hosts configured in the ansible inventory.
+    """
+    ansible_vault(env, command)
