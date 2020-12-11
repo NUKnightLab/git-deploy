a82e3282c333c4f99733eb756019b30000f91dd1
scott2b <scottbradley.mail@gmail.com>
2020-12-11 12:13:23 -0600

    adds type hints to config module

--- gitdeploy/config.py
+++ gitdeploy/config.py
@@ -1,15 +1,16 @@
 import os
 import yaml
+from typing import Dict
 from .repo import get_project_path
 
 
-def get_config_dir():
+def get_config_dir() -> str:
     return os.path.join(get_project_path(),
         os.environ.get('GIT_DEPLOY_PROJECT_CONFIG_DIR', 'deploy'))
 
 
 _common_config = None
-def get_common_config():
+def get_common_config() -> Dict:
     global _common_config
     if _common_config is None:
         fn = os.path.join(get_config_dir(), 'config.common.yml')
@@ -18,7 +19,7 @@ def get_common_config():
     return _common_config
 
 
-def get_env_config(env):
+def get_env_config(env:str) -> Dict:
     config = get_common_config()
     fn = os.path.join(get_config_dir(), f'config.{env}.yml')
     with open(fn) as f:
